98be84853c640aa1c3227f394ba84105c1697e8a
EiffL <fr.eiffel@gmail.com>
2023-01-16 04:16:24 +0100

    fixes properties of stars

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,16 +2,16 @@ particlesJS("intro", {"particles":{"number":{"value":100,"density":{"enable":tru
                                     "color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},
                                     "polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},
                                     "opacity":{"value":1.,"random":true,
-                                    "anim":{"enable":true,"speed":4.710047831347802,"opacity_min":0.1,"sync":false}},
-                                    "size":{"value":2,"random":true,"anim":{"enable":true,"speed":1,"size_min":0.1,"sync":false}},
+                                    "anim":{"enable":true,"speed":5,"opacity_min":0.1,"sync":false}},
+                                    "size":{"value":1.5,"random":true,"anim":{"enable":true,"speed":10,"size_min":0.2,"sync":false}},
                                     "line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},
-                                    "move":{"enable":true,"speed":1.,"direction":"none","random":false,"straight":false,"out_mode":"out",
+                                    "move":{"enable":true,"speed":0.1,"direction":"none","random":false,"straight":false,"out_mode":"out",
                                     "bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},
-                                    "interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"grab"},
+                                    "interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"bubble"},
                                     "onclick":{"enable":true,"mode":"push"},"resize":true},
                                     "modes":{"grab":{"distance":97.44926547616143,"line_linked":{"opacity":1}},
-                                    "bubble":{"distance":243.62316369040352,"size":4.060386061506726,"duration":2,"opacity":1,"speed":3},
-                                    "repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},
+                                    "bubble":{"distance":243.62316369040352,"size":2.,"duration":2,"opacity":1,"speed":3},
+                                    "repulse":{"distance":60.,"duration":5.},"push":{"particles_nb":1},"remove":{"particles_nb":2}}},
                                     "retina_detect":true});
 
     function effectsHomeSection(homeSection, scrollTopp) {
